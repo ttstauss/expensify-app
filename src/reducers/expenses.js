484463eee9a6f1bbdec163fa23a023b5c3ee4230
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-06-21 12:56:15 -0600

    Get firebase expenses on start

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -22,6 +22,8 @@ export default (state = expensesReducerDefaultSate, action) => {
           return expense
         }
       })
+    case 'SET_EXPENSES':
+      return action.expenses
     default:
       return state
   }
